10a1b8e54861af9763cc181b456925ce03286dec
Lxvia <oliviarudolf67@gmail.com>
2026-04-06 20:07:30 +0200

    ajout h2 bdd test

--- backend/src/test/java/com/collector/collector_backend/CollectorBackendApplicationTests.java
+++ backend/src/test/java/com/collector/collector_backend/CollectorBackendApplicationTests.java
@@ -2,12 +2,13 @@ package com.collector.collector_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CollectorBackendApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
